b8f6a7d8ca650b102d4ef1ee31f4850f16d789b8
Snehak1 <be_sneha_singh@outlook.com>
2017-06-20 16:32:03 +0530

    Add files via upload

---
+++ Assign_4.java
@@ -0,0 +1,13 @@
+
+public class Assign_4 {
+
+	public static void main(String[] args) {
+		System.out.println("ASCII Conversion");
+		for (int i=65;i<=90;i++)
+		{
+		char y=(char)i;
+		System.out.println(i+ " = " +y);
+		
+	}
+	}
+}
